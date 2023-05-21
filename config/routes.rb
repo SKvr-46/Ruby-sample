e5aa712fbd879d67fc52b1fbc9ec138bfdbe1951
SKvr-46 <koeich4@gmail.com>
2023-05-21 13:44:02 +0900

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   root to: 'site#index'
+
+  get 'events', to: 'site#index'
+  get 'events/new', to: 'site#index'
+  get 'events/:id', to: 'site#index'
+  get 'events/:id/edit', to: 'site#index'
+
+#/api/events/...にアクセス可能　apiはcontrollerにある
+    namespace :api do 
+      resources :events, only: %i[index show create destroy update]
+    end
 end
